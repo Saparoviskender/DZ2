93f52ded611a588f3f6d3a236e69b3f8c5b555c2
Saparoviskender <127577144+Saparoviskender@users.noreply.github.com>
2023-03-11 09:38:07 +0600

    Add files via upload

---
+++ Новая папка 6/index.js
@@ -0,0 +1,44 @@
+
+
+   // // 1)
+   // var arr = [11, 265, 777, 566, 209, 388, 555, 318, 22]
+   
+   // for (let i = 0; i < arr.length; i++) {
+   //   var num = String(arr[i]);
+   //   var suu = num[0];
+   //   if (suu == 1 || suu == 2 || suu == 5) {
+   //     console.log(num);
+   //   }
+   // }
+   
+// 
+// 
+   // 2)
+//    var numbers = [0,1,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+// for (let i = 20; i > 0; i--) {
+//    console.log(i);
+// } 
+
+
+// 3)
+// var colors = { 
+//    red: 'красный',
+//    yellow : 'желтый',
+//    green : 'зеленый'
+// }
+// var salam = prompt('введите цвет красный, желтый, зеленый').trim().toLowerCase()
+
+// switch (salam) {
+//    case colors.red:
+//       alert('STOP')
+//       break
+//     case colors.yellow:
+//       alert('WAIT');
+//       break
+//     case colors.green: 
+//       alert('START')
+//       break
+//    default:
+//       alert('ERROR')
+//       break
+// }
